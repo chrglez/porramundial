a1444b3809ced74e2bab3b748aa36c8e4e6010cd
Cristian González Martel <christian.gonzalez@ulpgc.es>
2022-12-02 17:42:18 +0000

    Actualización partidos

--- docs/code/rvest_clasificacion.r
+++ docs/code/rvest_clasificacion.r
@@ -3,9 +3,21 @@ library(rvest)
 library(dplyr)
 library(purrr)
 
+dict_countries <- read.csv("https://gist.githubusercontent.com/ideaalab/7caf20bc8fd6c625163927e2d478e05f/raw/b49e20c16ed35e3295c0442d4b85a13eea645cee/paises.csv",encoding = "UTF-8")
+
 
 contenido <- rvest::read_html("https://es.wikipedia.org/wiki/Copa_Mundial_de_F%C3%BAtbol_de_2022")
 
 tablas <- contenido %>%
 html_elements('.noprint.AP.rellink + table') %>%
 html_table()
+
+octavos <- contenido %>%
+    html_elements('.vevent + table')  %>%
+    html_table()
+
+partidos_octavos <- map_df(octavos[51:56],~ .x %>% select(c(2,4)) %>% slice(1))
+
+partidos_cuartos <- map_df(octavos[57:60],~ .x %>% select(c(2,4)) %>% slice(1))
+
+partidos_semis <- map_df(octavos[61:62],~ .x %>% select(c(2,4)) %>% slice(1))
